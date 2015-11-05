17593a67003ffb95bc4ccc5e7a334baedeade953
nrewik <nrewik@outlook.com>
2015-11-05 17:03:36 +0700

    Add Xcode project

---
+++ Flux-in-Swift-ExampleTests/Flux_in_Swift_ExampleTests.swift
@@ -0,0 +1,36 @@
+//
+//  Flux_in_Swift_ExampleTests.swift
+//  Flux-in-Swift-ExampleTests
+//
+//  Created by Nutchaphon Rewik on 05/11/2015.
+//  Copyright © 2015 Nutchaphon Rewik. All rights reserved.
+//
+
+import XCTest
+@testable import Flux_in_Swift_Example
+
+class Flux_in_Swift_ExampleTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
